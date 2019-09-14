5b8d337172836622bfd05f0d9abd288cd169eb4a
Yana Petrova <janavpetrova@gmail.com>
2019-09-14 16:48:22 +0300

    improve

--- test/controllers/recipes_controller_test.exs
+++ test/controllers/recipes_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule Sandwich.RecipesControllerTest do
   use Sandwich.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET :index", %{conn: conn} do
     conn = get conn, recipes_path(conn, :index)
     assert html_response(conn, 200)
   end
