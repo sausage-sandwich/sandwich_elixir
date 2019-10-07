f9d2ab1c0a948bc83d11c7bb746c582b1523147c
Yana Petrova <janavpetrova@gmail.com>
2019-10-07 23:15:55 +0300

    add ingredients controller tests (#7)

---
+++ test/controllers/ingredients_controller_test.exs
@@ -0,0 +1,24 @@
+defmodule Sandwich.IngredientsControllerTest do
+  use Sandwich.ConnCase
+
+  test "GET :index", %{conn: conn} do
+    conn = get conn, ingredients_path(conn, :index)
+
+    assert html_response(conn, 200)
+  end
+
+  test "GET :new", %{conn: conn} do
+    conn = get conn, ingredients_path(conn, :new)
+
+    assert html_response(conn, 200)
+  end
+
+  test "POST :create", %{conn: conn} do
+    ingredient_title = "carrot"
+
+    conn = post conn, ingredients_path(conn, :create, %{"ingredient" => %{"title" => ingredient_title}})
+
+    assert html_response(conn, 302)
+    assert Sandwich.Repo.get_by(Sandwich.Ingredient, title: ingredient_title)
+  end
+end
