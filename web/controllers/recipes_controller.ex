f3a85b7e0034f685c709196ac64651c33e06dba8
Yana Petrova <janavpetrova@gmail.com>
2019-10-27 16:47:29 +0300

    add edit and update actions

--- web/controllers/recipes_controller.ex
+++ web/controllers/recipes_controller.ex
@@ -1,32 +1,26 @@
 defmodule Sandwich.RecipesController do
   use Sandwich.Web, :controller
+  alias Sandwich.Repo
+  alias Sandwich.Recipe
 
   def index(conn, _params) do
-    alias Sandwich.Repo
-    alias Sandwich.Recipe
     recipes = Repo.all(Recipe)
 
     render conn, "index.html", recipes: recipes
   end
 
   def show(conn, params) do
-    alias Sandwich.Repo
-    alias Sandwich.Recipe
-
     recipe = Repo.preload(Repo.get_by(Recipe, id: params["id"]), :ingredients)
 
     render conn, "show.html", recipe: recipe
   end
 
   def new(conn, _params) do
-    alias Sandwich.Recipe
     changeset = Recipe.changeset(%Recipe{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"recipe" => recipe_params}) do
-    alias Sandwich.Recipe
-    alias Sandwich.Repo
     %Recipe{}
     |> Recipe.changeset(recipe_params)
     |> Repo.insert()
@@ -37,6 +31,28 @@ defmodule Sandwich.RecipesController do
         |> redirect(to: recipes_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, params) do
+    recipe = Repo.get_by(Recipe, id: params["id"])
+    changeset = Recipe.changeset(recipe, %{})
+
+    render(conn, "edit.html", recipe: recipe, changeset: changeset)
+  end
+
+  def update(conn, %{"recipe" => recipe_params, "id" => id}) do
+    recipe = Repo.get_by(Recipe, id: id)
+    recipe
+    |> Recipe.changeset(recipe_params)
+    |> Repo.update()
+    |> case do
+    {:ok, recipe} ->
+      conn
+      |> put_flash(:info, "Recipe updated")
+      |> redirect(to: recipes_path(conn, :show, recipe.id))
+    {:error, %Ecto.Changeset{} = changeset} ->
+      render(conn, "edit.html", changeset: changeset, recipe: recipe)
     end
   end
 end
