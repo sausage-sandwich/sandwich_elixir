5c58906c9b380093c4593364aca2f8a12117ae68
Yana Petrova <janavpetrova@gmail.com>
2020-01-14 22:49:14 +0300

    fix tests

--- test/controllers/recipes_controller_test.exs
+++ test/controllers/recipes_controller_test.exs
@@ -33,17 +33,17 @@ defmodule Sandwich.RecipesControllerTest do
         recipe: %{
           body: "body",
           title: recipe_title,
-          ingredients: [
-            %{id: ingredient.id}
-          ]
+          recipe_ingredients: %{
+            "0" => %{ingredient_id: ingredient.id, quantity: 100.0, unit: "г"}
+          }
         }
       }
 
-    created_recipe = Repo.preload(Repo.get_by(Recipe, title: recipe_title), :ingredients)
+    created_recipe = Repo.preload(Repo.get_by(Recipe, title: recipe_title), recipe_ingredients: :ingredient)
 
     assert html_response(conn, 302)
     assert created_recipe
-    assert Enum.member?(created_recipe.ingredients, ingredient)
+    assert Enum.member?(Enum.map(created_recipe.recipe_ingredients, &(&1.ingredient)), ingredient)
   end
 
   test "GET :edit", %{conn: conn} do
